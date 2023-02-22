d6faea67770ca171187a0b1c3ee8cdb38658847f
Francesco Franzoni <francesco.franzoni@outlook.com>
2023-02-22 18:56:29 +0100

    Secondo Commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources\css\app.css', 'resources\js\app.js'])
+    <title>{{ $title ?? 'Presto.it' }}</title>
+</head>
+
+<body>
+    <header>
+        <x-navBar />
+    </header>
+    {{ $slot }}
+
+
+
+    <x-footer />
+
+
+
+</body>
+
+</html>
